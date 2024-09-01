7b72c4206c37c968a307dfb7cd969cacbc7e1a7d
jagadishchakma <jagadishchakma401@gmail.com>
2024-09-01 22:57:59 +0600

    image not found

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
